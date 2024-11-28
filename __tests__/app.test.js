5d1f096ace4c2c091776c7af633e60b975adc1af
fdesai60 <fdesai60@gmail.com>
2024-11-28 09:41:58 +0000

    getApiArticle endpoint comment count property added

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -48,18 +48,19 @@ describe("GET /api/topics",()=>{
 describe("/api/articles/:article_id",()=>{
   test("200: Responds with an article object",()=>{
     return request(app)
-    .get("/api/articles/2")
+    .get("/api/articles/1")
     .expect(200)
     .then(({body: {article}})=>{
       expect(article).toMatchObject({
-        author:"icellusedkars",
-        title:"Sony Vaio; or, The Laptop",
-        article_id:2,
-        body:"Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.",
-        topic:"mitch",
+        author:expect.any(String),
+        title:expect.any(String),
+        article_id:1,
+        body:expect.any(String),
+        topic:expect.any(String),
         created_at:expect.any(String),
         votes:expect.any(Number),
-        article_img_url:"https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700"
+        article_img_url:expect.any(String),
+        comment_count:"11"
       })
     })
   })
@@ -504,5 +505,6 @@ describe(" GET /api/articles (topic query)",()=>{
       })
   })
 
-
 })
+
+
